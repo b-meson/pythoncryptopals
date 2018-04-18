7830ff01360631fcdf1170d6d6c7d8a903b34b54
Freddy Martinez <freddymartinez9@gmail.com>
2018-04-18 11:34:33 -0700

    a working dictionary that returns a ranking per byte

--- set1/challenge3.py
+++ set1/challenge3.py
@@ -5,37 +5,38 @@
 
 
 def xor_singlekey_singlebyte(input_b, single_key):
-    bytes_out = b''
+    bytes_out = bytearray()
     for x in input_b:
-        bytes_out += bytes([x ^ single_key])
+        bytes_out.append(x ^ single_key)
     try:
-        english_character_freq(bytes_out)
+        return english_character_freq(bytes_out)
     except UnicodeDecodeError:
         pass
 
 
-#operate on raw bytes, decode string, multiple the value, then rank.
-
+# operate on raw bytes, decode string, multiple the value, then rank.
 def english_character_freq(decoded_bytes):
     english_map = {'E': 12.02, 'T': 9.10, 'A': 8.12, 'O': 7.68, 'I': 7.31, 'N': 6.95, 'S': 6.28, 'R': 6.02, 'H': 5.92,
                    'D': 4.32, 'L': 3.98, 'U': 2.88, 'C': 2.71, 'M': 2.61, 'F': 2.30, 'Y': 2.11, 'W': 2.09, 'G': 2.03,
                    'P': 1.82, 'B': 1.49, 'V': 1.11, 'K': 0.69, 'X': 0.17, 'Q': 0.11, 'J': 0.10, 'Z': 0.07}
+    weight = 0
     for b in decoded_bytes:
+        s = chr(b)
         for key, value in english_map.items():
-            rank = int()
-            b = chr(b)
-            ranking = {b: rank}
-            if b == key.lower():
-                rank = (value * .01)
-            else:
-                rank = 0
-            return ranking
-            print(ranking)
+            key = key.lower()
+            if key == s.lower():
+                weight = weight + (value * .01)
+    weight = weight * 100
+    return weight
 
 # The xor is against a single byte from 0 to 256, not a literal ascii "character"
 def xor_singlekey_allpermuations(input_b):
+    x = {}
     for i in range(256):
-        xor_singlekey_singlebyte(input_b, i)
+        returned_weight = xor_singlekey_singlebyte(input_b, i)
+        x.update([(i, returned_weight)])
+    print(x)
+    return x
 
 
 def test_xor_singlekey():
